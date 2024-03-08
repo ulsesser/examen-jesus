8f85998047e78770368bbc58171a7abd31a273a8
ulsesser <152214803+ulsesser@users.noreply.github.com>
2024-03-08 15:56:27 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+app.use(express.json({limit:'50mb'}));
+app.use(express.urlencoded({limit:'50mb', extended: true}));
+app.listen(4000, ()=>{
+    console.log("ulises serna")
+});
+
+export default app;
+
+
+
